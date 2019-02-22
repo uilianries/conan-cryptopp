a18a5d14de61c97ae012a5e267dc5d8fe7a24137
Uilian Ries <uilianries@gmail.com>
2019-02-22 16:10:15 -0300

    add shippable [skip travis] [skip appveyor]

--- build.py
+++ build.py
@@ -4,7 +4,7 @@
 from cpt.packager import ConanMultiPackager
 
 if __name__ == "__main__":
-    entry_script = "pwd; ls; ls /root/src/github.com/uilianries"
+    entry_script = "pwd; ls; sudo ls /root/src/github.com/uilianries"
     builder = ConanMultiPackager(username="uilianries", docker_entry_script=entry_script)
     builder.add_common_builds(pure_c=False)
     builder.run()
