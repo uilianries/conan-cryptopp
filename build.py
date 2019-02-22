b6829543e741473a21d2bb02b5bf49364bb1333e
Uilian Ries <uilianries@gmail.com>
2019-02-22 17:19:54 -0300

    add shippable [skip travis] [skip appveyor]

--- build.py
+++ build.py
@@ -4,7 +4,7 @@
 from cpt.packager import ConanMultiPackager
 
 if __name__ == "__main__":
-    entry_script = "git clone https://github.com/uilianries/conan-cryptopp.git /home/conan/project && cd /home/conan/project && git checkout testing/5.6.5 && cd /home/conan"
-    builder = ConanMultiPackager(username="uilianries", docker_entry_script=entry_script)
+    #entry_script = "git clone https://github.com/uilianries/conan-cryptopp.git /home/conan/project && cd /home/conan/project && git checkout testing/5.6.5 && cd /home/conan"
+    builder = ConanMultiPackager(username="uilianries")
     builder.add_common_builds(pure_c=False)
     builder.run()
